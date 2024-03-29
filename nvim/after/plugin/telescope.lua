af60a9085dcd2ff146cea528147b93eb8fda1e1d
Tarek Kekhia <tarekkekhia@Tareks-MacBook-Pro.local>
2024-03-29 09:27:25 -0400

    telescope remap to open files in buffer

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -14,5 +14,6 @@ require('telescope').setup{
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})

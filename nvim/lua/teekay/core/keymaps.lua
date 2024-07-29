3fc12d44df6286bdedea734ac335b0e61083c238
Tarek Kekhia <tarekkekhia@Tareks-MacBook-Pro.local>
2024-07-28 20:18:07 -0400

    fixed treesitter autotag

--- nvim/lua/teekay/core/keymaps.lua
+++ nvim/lua/teekay/core/keymaps.lua
@@ -12,12 +12,11 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- delete and move cursor to desired location
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- not sure what these are for yet...
 vim.keymap.set("n", "Q", "<nop>")
@@ -34,11 +33,5 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
-vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
-
--- shoutout changes
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
-
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
+vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>")

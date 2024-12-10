1990d44d4ca4bd2d57aab8ed942d27bed0f77f9c
Tarek Kekhia <tarekkekhia@Tareks-MacBook-Pro.local>
2024-12-10 11:00:00 -0500

    h,l motion keys should not be remapped... duhhh

--- nvim/lua/teekay/core/markdown.lua
+++ nvim/lua/teekay/core/markdown.lua
@@ -6,15 +6,11 @@ vim.api.nvim_create_autocmd("FileType", {
 		-- Normal mode mappings
 		vim.keymap.set("n", "j", "gj", { buffer = true })
 		vim.keymap.set("n", "k", "gk", { buffer = true })
-		vim.keymap.set("n", "h", "gh", { buffer = true })
-		vim.keymap.set("n", "l", "gl", { buffer = true })
 		vim.keymap.set("n", "$", "g$", { buffer = true })
 		vim.keymap.set("n", "0", "g0", { buffer = true })
 		-- Visual mode mappings
 		vim.keymap.set("v", "j", "gj", { buffer = true })
 		vim.keymap.set("v", "k", "gk", { buffer = true })
-		vim.keymap.set("v", "h", "gh", { buffer = true })
-		vim.keymap.set("v", "l", "gl", { buffer = true })
 		vim.keymap.set("v", "$", "g$", { buffer = true })
 		vim.keymap.set("v", "0", "g0", { buffer = true })
 	end,

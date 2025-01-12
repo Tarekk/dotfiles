b624f300a8de436128603139aa50aa453107a8b4
Tarek Kekhia <tarek.kekhia@gmail.com>
2025-01-12 12:13:26 -0500

    md editing enhancements

--- nvim/lua/teekay/core/markdown.lua
+++ nvim/lua/teekay/core/markdown.lua
@@ -3,12 +3,28 @@ vim.api.nvim_create_autocmd("FileType", {
 	callback = function()
 		vim.opt_local.wrap = true
 		vim.opt_local.linebreak = true
-
 		vim.opt_local.formatoptions:append("ro")
 		vim.opt_local.comments = "b:-"
 		vim.opt_local.breakindent = true
 		vim.opt_local.breakindentopt = "shift:2"
-
+		-- Function to indent unindented bullet points
+		local function indent_unindented_bullets()
+			local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+			local modified_lines = {}
+			for _, line in ipairs(lines) do
+				if line:match("^%-") then
+					table.insert(modified_lines, "  " .. line)
+				else
+					table.insert(modified_lines, line)
+				end
+			end
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, modified_lines)
+		end
+		-- Create autocmds for buffer
+		vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
+			buffer = 0,
+			callback = indent_unindented_bullets,
+		})
 		-- Normal mode mappings
 		vim.keymap.set("n", "j", "gj", { buffer = true })
 		vim.keymap.set("n", "k", "gk", { buffer = true })

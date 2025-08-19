0fef1c3656aba31f498f3f23936c65dcee3c74ca
emeraldtarek <tarek.kekhia@emeraldlake.io>
2025-08-19 12:48:41 -0400

    fix nested md indent

--- nvim/lua/teekay/core/markdown.lua
+++ nvim/lua/teekay/core/markdown.lua
@@ -12,23 +12,23 @@ vim.api.nvim_create_autocmd("FileType", {
 		vim.opt_local.breakindentopt = "list:2" -- Use list mode with negative shift to align with text
 
 		-- Function to indent unindented bullet points
-		local function indent_unindented_bullets()
-			local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
-			local modified_lines = {}
-			for _, line in ipairs(lines) do
-				if line:match("^%-[^-]") then -- Match single dash not followed by another dash
-					table.insert(modified_lines, "  " .. line)
-				else
-					table.insert(modified_lines, line)
-				end
-			end
-			vim.api.nvim_buf_set_lines(0, 0, -1, false, modified_lines)
-		end
-		-- Create autocmds for buffer
-		vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
-			buffer = 0,
-			callback = indent_unindented_bullets,
-		})
+		-- local function indent_unindented_bullets()
+		-- 	local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+		-- 	local modified_lines = {}
+		-- 	for _, line in ipairs(lines) do
+		-- 		if line:match("^%-[^-]") then -- Match single dash not followed by another dash
+		-- 			table.insert(modified_lines, "  " .. line)
+		-- 		else
+		-- 			table.insert(modified_lines, line)
+		-- 		end
+		-- 	end
+		-- 	vim.api.nvim_buf_set_lines(0, 0, -1, false, modified_lines)
+		-- end
+		-- -- Create autocmds for buffer
+		-- vim.api.nvim_create_autocmd({ "BufWritePre", "InsertLeave" }, {
+		-- 	buffer = 0,
+		-- 	callback = indent_unindented_bullets,
+		-- })
 		-- Normal mode mappings
 		vim.keymap.set("n", "j", "gj", { buffer = true })
 		vim.keymap.set("n", "k", "gk", { buffer = true })
